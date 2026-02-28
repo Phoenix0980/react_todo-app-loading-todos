5fba57b992af947e865bc7bd952ee036e7ac02bb
Andriy <andrijs373@gmail.com>
2026-02-28 14:01:28 +0200

    add task solution

--- src/api/todos.ts
+++ src/api/todos.ts
@@ -7,4 +7,18 @@ export const getTodos = () => {
   return client.get<Todo[]>(`/todos?userId=${USER_ID}`);
 };
 
-// Add more methods here
+// helpers for other parts of the application
+export const createTodo = (title: string) =>
+  client.post<Todo>('/todos', {
+    userId: USER_ID,
+    title,
+    completed: false,
+  });
+
+export const deleteTodo = (id: number) =>
+  client.delete(`/todos/${id}`);
+
+export const updateTodo = (
+  id: number,
+  updates: Partial<Pick<Todo, 'title' | 'completed'>>,
+) => client.patch<Todo>(`/todos/${id}`, updates);
